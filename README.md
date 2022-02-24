e801c963336e62b0fc0c43ff20da85493f22d4a7
jiaohongtao <hongtao.jiao@eaxboxdata.com>
2022-02-24 13:57:43 +0800

    PreparedStatement测试

--- README.md
+++ README.md
@@ -2,7 +2,19 @@ SpringBatch 技术测试项目
 [参考连接](https://www.cnblogs.com/jian0110/p/10838744.html)
 
 测试结果：
+SpringBatch
 1000  1s134ms
 5000  3s838ms
 10000 6s66ms
 50000 21s977ms
+
+[参考链接](https://qinyi.blog.csdn.net/article/details/108191988)
+PreparedStatement
+rewriteBatchedStatements=true (**必须打开**, 否则就不能用到批量插入的功能，跟单条插入无异)
+50000   2.298ms
+100000  2.331ms 4.308ms 2.288 seconds
+500000  2.274ms
+1000000 2.283ms
+2000000 2.309ms
+
+测试时间为数据库执行时间
--- src/test/java/com/example/BatchTest.java
+++ src/test/java/com/example/BatchTest.java
@@ -10,6 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  * @author jiaohongtao
  * @version 1.0.0
@@ -31,4 +36,59 @@ public class BatchTest {
                 .toJobParameters();
         simpleJobLauncher.run(importJob, jobParameters);
     }
+
+    @Test
+    public void testPre() {
+        //1.导入驱动jar包
+        //2.注册驱动(mysql5之后的驱动jar包可以省略注册驱动的步骤)
+        //Class.forName("com.mysql.jdbc.Driver");
+        //3.获取数据库连接对象
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        {
+            try {
+                //"&rewriteBatchedStatements=true",一次插入多条数据，只插入一次
+                String url = "jdbc:mysql://10.1.3.24:3306/jiaobk001?" + "&rewriteBatchedStatements=true";
+                conn = DriverManager.getConnection(url, "root", "123456");
+                //4.定义sql语句
+                String sql = "insert into person(name,age, gender) values(?,?,?)";
+                //5.获取执行sql的对象PreparedStatement
+                pstmt = conn.prepareStatement(sql);
+                //6.不断产生sql
+                int cnt = 2000000;
+                for (int i = 0; i < cnt; i++) {
+                    pstmt.setString(1, "name" + i);
+                    pstmt.setInt(2, 1000);
+                    pstmt.setString(3, "M");
+                    pstmt.addBatch();
+                }
+                //7.往数据库插入一次数据
+                pstmt.executeBatch();
+                System.out.println("添加" + cnt + "条信息成功！");
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                //8.释放资源
+                //避免空指针异常
+                if (pstmt != null) {
+                    try {
+                        pstmt.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (conn != null) {
+                    try {
+                        conn.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+
 }
